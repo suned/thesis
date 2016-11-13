374c1ede5935c1369a5c8125a770bbd3e23de247
Sune Debel <sune@Sunes-MacBook-Pro.local>
2016-11-13 20:08:31 +0100

    fix script

--- experiment/src/main/python/plot.py
+++ experiment/src/main/python/plot.py
@@ -1,30 +1,21 @@
-font_size = 10
-
 import matplotlib as mpl
-
-mpl.use("pgf")
-pgf_with_rc_fonts = {
-    "font.size": font_size,
-    "pgf.rcfonts": False,
-    "pgf.preamble": [
-        "\\usepackage{eulervm}",
-        "\\usepackage{mathpazo}"
-    ]
-}
-mpl.rcParams.update(pgf_with_rc_fonts)
-import matplotlib.pyplot as plt
+from cycler import cycler
+import numpy as np
+mpl.rcParams['axes.prop_cycle'] = cycler('color', ['#268bd2', '#2aa198', '#859900'])
+from matplotlib import pyplot as plt
+font_size = 10
 
 plt.figure(figsize=(5,3))
 plt.plot(range(5), label="$\\Delta(a)$")
+plt.plot(range(5), np.sin(range(5)), label="$\\beta(a)$")
 plt.xlabel(u"µ is not $\\mu$")
 plt.ylabel("this is the y label")
 plt.gca().spines["top"].set_visible(False)
 plt.gca().spines["right"].set_visible(False)
 plt.gca().xaxis.set_ticks_position("bottom")
 plt.gca().yaxis.set_ticks_position("left")
-labels = plt.gca().yaxis.get_ticklabels()
-labels[0] = ""
-plt.gca().set_yticklabels(labels)
-plt.legend(loc='lower right', frameon=False, prop={'size': font_size})
+labels = plt.gca().yaxis.get_major_ticks()
+labels[0].label1.set_visible(False)
+plt.legend(loc='upper left', frameon=False, prop={'size': font_size})
 plt.tight_layout()
 plt.savefig("img/test.pgf")
