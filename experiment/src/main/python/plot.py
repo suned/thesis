720026497de5c27f438838a5f32e771ab9efdc5b
Sune Debel <sune@Sunes-MacBook-Pro.local>
2016-11-13 20:32:53 +0100

    finish plot script

--- experiment/src/main/python/plot.py
+++ experiment/src/main/python/plot.py
@@ -3,19 +3,15 @@ from cycler import cycler
 import numpy as np
 mpl.rcParams['axes.prop_cycle'] = cycler('color', ['#268bd2', '#2aa198', '#859900'])
 from matplotlib import pyplot as plt
-font_size = 10
 
-plt.figure(figsize=(5,3))
 plt.plot(range(5), label="$\\Delta(a)$")
 plt.plot(range(5), np.sin(range(5)), label="$\\beta(a)$")
-plt.xlabel(u"µ is not $\\mu$")
+plt.xlabel("this is the x label")
 plt.ylabel("this is the y label")
-plt.gca().spines["top"].set_visible(False)
-plt.gca().spines["right"].set_visible(False)
 plt.gca().xaxis.set_ticks_position("bottom")
 plt.gca().yaxis.set_ticks_position("left")
 labels = plt.gca().yaxis.get_major_ticks()
 labels[0].label1.set_visible(False)
-plt.legend(loc='upper left', frameon=False, prop={'size': font_size})
+plt.legend(loc='upper left')
 plt.tight_layout()
 plt.savefig("img/test.pgf")
