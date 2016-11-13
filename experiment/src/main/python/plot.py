ad341a21a14a56a8aedae938359eb135e64a6728
Sune Debel <sune@Sunes-MacBook-Pro.local>
2016-11-13 17:47:48 +0100

    make mpl script for latex format

---
+++ experiment/src/main/python/plot.py
@@ -0,0 +1,30 @@
+font_size = 10
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.use("pgf")
+pgf_with_rc_fonts = {
+    "font.size": font_size,
+    "pgf.rcfonts": False,
+    "pgf.preamble": [
+        "\\usepackage{eulervm}",
+        "\\usepackage{mathpazo}"
+    ]
+}
+mpl.rcParams.update(pgf_with_rc_fonts)
+
+
+plt.figure(figsize=(5,3))
+plt.plot(range(5), label="$\\Delta(a)$")
+plt.xlabel(u"µ is not $\\mu$")
+plt.ylabel("this is the y label")
+plt.tight_layout(0)
+plt.gca().spines["top"].set_visible(False)
+plt.gca().spines["right"].set_visible(False)
+plt.gca().xaxis.set_ticks_position("bottom")
+plt.gca().yaxis.set_ticks_position("left")
+labels = plt.gca().yaxis.get_ticklabels()
+labels[0] = ""
+plt.gca().set_yticklabels(labels)
+plt.legend(loc='lower right', frameon=False, prop={'size': font_size})
+plt.savefig("img/test.pgf")
