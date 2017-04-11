86dd3a0cf2aae551658230a9761fb4c6788aa594
Sune Debel <sad@archii.it>
2017-04-11 15:51:12 +0200

    rename result

--- experiment/mtl_relation_extraction/config.py
+++ experiment/mtl_relation_extraction/config.py
@@ -2,7 +2,7 @@ import numpy
 
 semeval_train_path = "semeval/train.txt"
 semeval_test_path = "semeval/test.txt"
-out_path = "out"
+out_path = "results"
 ace_path = "ace"
 max_len_buffer = 1
 random_state = 1
