273ba51176d9996e1902bef4183d874025fa48c7
Sune Debel <sad@archii.it>
2017-05-15 11:11:20 +0200

    add run script

--- run.sh
+++ run.sh
@@ -1,4 +1,41 @@
 #!/usr/bin/env bash
 
-python -m experiment --save SemEval --auxiliary-tasks none
-python -m experiment --save SemEval+ACE --auxiliary-tasks ACE
+python -m experiment \
+  --save SemEval \
+  --auxiliary-tasks none
+
+python -m experiment \
+  --save SemEval+ACE \
+  --auxiliary-tasks ACE
+
+python -m experiment \
+  --save SemEval+ACE_sequential \
+  --auxiliary-tasks ACE \
+  --fit-sequential
+
+python -m experiment \
+  --save SemEval+Conll2000POS \
+  --auxiliary-tasks Conll2000POS
+
+python -m experiment \
+  --save SemEval+Conll2000POS_sequential \
+  --auxiliary-tasks Conll2000POS \
+  --fit-sequential
+
+python -m experiment \
+  --save SemEval+Conll2000Chunk \
+  --auxiliary-tasks Conll2000Chunk
+
+python -m experiment \
+  --save SemEval+Conll2000Chunk_sequential \
+  --auxiliary-tasks Conll2000Chunk \
+  --fit-sequential
+
+python -m experiment \
+  --save SemEval+GMB-NER \
+  --auxiliary-tasks GMB-NER
+
+python -m experiment \
+  --save SemEval+GMB-NER_sequential \
+  --auxiliary-tasks GMB-NER \
+  --fit-sequential
