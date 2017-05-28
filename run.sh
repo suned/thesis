3baf76c21c00dbc6c4510a2de4a08596cd42bf03
Sune Debel <sad@archii.it>
2017-05-28 17:25:57 +0200

    udpate run.sh

--- run.sh
+++ run.sh
@@ -2,20 +2,25 @@
 
 python -m experiment \
   --save SemEval \
-  --auxiliary-tasks none
+  --auxiliary-tasks none \
+  --share-filters
 
 python -m experiment \
   --save SemEval+ACE \
-  --auxiliary-tasks ACE
+  --auxiliary-tasks ACE \
+  --share-filters
 
 python -m experiment \
   --save SemEval+Conll2000POS \
-  --auxiliary-tasks Conll2000POS
+  --auxiliary-tasks Conll2000POS \
+  --share-filters
 
 python -m experiment \
   --save SemEval+Conll2000Chunk \
-  --auxiliary-tasks Conll2000Chunk
+  --auxiliary-tasks Conll2000Chunk \
+  --share-filters
 
 python -m experiment \
   --save SemEval+GMB-NER \
-  --auxiliary-tasks GMB-NER
+  --auxiliary-tasks GMB-NER \
+  --share-filters
